b7f201cbebbd4ee14996b341826880f21408a325
Rostyslav <124372762+hockag@users.noreply.github.com>
2024-04-03 21:31:11 +0300

    save

---
+++ script.js
@@ -0,0 +1,53 @@
+// let i = 0;
+// while(i <= 10) {
+//     console.log(i);
+//     i++
+// }
+// //1
+//
+// for (let i = 0; i <= 20; i++) {
+//     if (i % 2 !== 0 ) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+// //2
+//
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`7 * ${i} = ${7 * i}`);
+// }
+// //3
+
+// const numbers = [1,2,3,4,5,6];
+// let i = 0 ;
+
+// while (i < numbers.length) {
+//     console.log(numbers[i]);
+//     i++
+// }
+
+// let numbers = [1,2,3,4,5,6,7,8,9,10];
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] === 7) {
+//         break;
+//     }
+//     console.log(numbers[i]);
+// }
+
+// let n = 10;
+// for (let i = 0; i < n; i++) {
+//     if (i >= n) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// let i = 1;
+// while (i <= 20) {
+//     if (i % 3 === 0) {
+//         i++;
+//         continue;
+//     }
+//     console.log(i);
+//     i++
+//  }
